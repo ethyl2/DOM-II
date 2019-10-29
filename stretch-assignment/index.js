5d4e222f3e96dcf7bf963869a10e35d085b04493
Heather Nuffer <zzzuzz@gmail.com>
2019-10-29 17:02:16 -0600

    Added 2 stretch goals to stretch project

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -8,40 +8,39 @@ allBlocks.forEach(block => {
     block.addEventListener('click', function(event) {
         allBlocks.forEach(one => one.style.order = 0);
         console.log("in click event");
-        //event.target.style.position = "relative";
-        //TweenMax.to(event.target, 3, {top: "50%"});
         event.target.style.order = -1;
-        //setTimeout(() => {event.target.style.order = -1;}, 5000);
     });
 
     //While a mouse is clicked down on a box, it should move to the right
     // (It should continuously move, there is no limit on how far it will go).
+    
     let interval;
     let pos =10;
     block.addEventListener('mousedown', function(event) {
-        //console.log(event.target);
         event.stopPropagation();
         interval = setInterval(function() {
-            //console.log("mouse is down");
-            pos++;
+            //Stretch: Give the travelers a limit on the distance it can travel.
+            if (pos < 600) {
+                pos++;
+            }
             event.target.style.marginLeft = pos + "px";
         }, 10);
     }, {
         capture: true
     });
 
-    //TODO: Stretch: When the mouse button is released, 
+    //Stretch: When the mouse button is released, 
     // it should travel back to its original position.
     block.addEventListener('mouseup', function(event) {
-        clearInterval(interval);
-        //event.target.style.marginLeft = 10;
-        console.log("in mouse up");
+       clearInterval(interval);
+       event.target.style.marginLeft = "10px";
+       pos = 0;
     });
     
+    // When mouse leaves block, the block stops moving.
     block.addEventListener('mouseleave', function(event) {
         clearInterval(interval);
         event.target.style.marginLeft = 10;
-        //console.log("in mouse leave");
     });
     
 });
